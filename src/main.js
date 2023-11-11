cffd8de06a7cca0ae5ffe6a87b6e8069703ece93
Alex Matsuev <alex.matsuev@gmail.com>
2023-11-11 16:44:46 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import App from './App.vue'
 const pinia = createPinia()
 const app = createApp(App)
 
+// bjdhkgf kasdgfjk  gasdfg adfhs
+// gasdhjfg asdjkgf asdg fjkfag dfsghj
+
+
 app.use(pinia)
 app.use(ElementPlus)
 app.mount('#lesson01')
